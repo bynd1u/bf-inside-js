c0d53fc9a499217a67bb9d3ec55224a028fa1a3e
bynd1u <semenbindu@gmail.com>
2025-03-04 11:14:32 +0100

    loops repeat-string ex

--- lesson-plans/for-loops/2-integrate/repeat-string/script.js
+++ lesson-plans/for-loops/2-integrate/repeat-string/script.js
@@ -1,11 +1,20 @@
 import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("repeat-it").addEventListener('click', () => {
   debugger;
 
   // read user values
+  let str = readString("user-text");
+  let num = readNumber("number-of-times");
 
+  let result = "";
   // repeat the string
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < str.length; j++) {
+      result += str[j];
+    }
+  }
 
   // display the repeated string
+  display("repeated-output", result);
 });
