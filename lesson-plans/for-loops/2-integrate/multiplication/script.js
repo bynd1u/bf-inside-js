beba795951cfc6e57fc2bd33faa16881a63c9bc4
bynd1u <semenbindu@gmail.com>
2025-03-04 10:59:58 +0100

    loops multiplication ex

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -1,11 +1,18 @@
 import { readNumber, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("do-math").addEventListener('click', () => {
   debugger;
 
   // read user values
-
+  let num1 = readNumber("left");
+  let num2 = readNumber("right")
   // use a for loop to multiply the two numbers
-
+  let result = 0;
+  for (let i = 0; i < num2; i++) {
+    result += num1;
+  }
+  
   // display the product
+  display("product", result);
+  
 });
