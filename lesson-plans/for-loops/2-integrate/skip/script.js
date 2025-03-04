bf03ab9a90bb5a335d93b7ba766c7896db99c49e
bynd1u <semenbindu@gmail.com>
2025-03-04 11:58:17 +0100

    loops skip ex

--- lesson-plans/for-loops/2-integrate/skip/script.js
+++ lesson-plans/for-loops/2-integrate/skip/script.js
@@ -1,11 +1,21 @@
 import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("skip-them").addEventListener('click', () => {
   debugger;
 
   // read user values
+  let num = readNumber("skip-size");
+  let str = readString("user-text");
 
+
+
+  let result = "";
   // create a new string with skipped characters
 
+  for (let i = 0; i < str.length; i += num) {
+    result += str[i];
+  }
+
   // display the skipped string
+  display("skipped-output", result);
 });
