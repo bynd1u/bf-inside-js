856f2ec854ea31056d900ecf770aa1ba3f429b37
bynd1u <semenbindu@gmail.com>
2025-03-04 11:20:29 +0100

    loops reverse-string ex

--- lesson-plans/for-loops/2-integrate/reverse-string/script.js
+++ lesson-plans/for-loops/2-integrate/reverse-string/script.js
@@ -1,11 +1,16 @@
 import { readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("reverse-it").addEventListener('click', () => {
   debugger;
 
   // read user text
+  let str = readString("user-text");
 
+  let result = "";
   // use a for loop that counts down (i--) to reverse the input
-
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
   // display the reversed string
+  display("reversed-output", result);
 });
